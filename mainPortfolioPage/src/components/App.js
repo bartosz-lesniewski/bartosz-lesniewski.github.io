9a9cdc424b2f8e539b0efcb35de45653652ac703
bartosz-lesniewski <b.lesniewski8@gmail.com>
2021-03-05 21:46:02 +0100

    fresh start after bootcamp

---
+++ mainPortfolioPage/src/components/App.js
@@ -0,0 +1,12 @@
+import "../styles/app.css"
+
+function App() {
+  return (
+    <div className="main-container">
+    <h1>Hi, my name is Bartosz Leśniewski</h1>
+    <h2>and I am front-end developer.</h2>
+    </div>
+  );
+}
+
+export default App;
